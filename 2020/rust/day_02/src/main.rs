d9f9bf2bcc9798c89303b89d85ae2e5391499688
Daniel Antrim <dantrim@lbl.gov>
2020-12-02 11:30:02 -0800

    rustfmt on day 2 solution

--- 2020/rust/day_02/src/main.rs
+++ 2020/rust/day_02/src/main.rs
@@ -8,7 +8,7 @@
 
 use clap::{App, Arg};
 use std::fs::File;
-use::std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 fn read_data_from_file(path: &str) -> Vec<String> {
@@ -23,49 +23,69 @@ struct DBEntry<'a> {
     min: i32,
     max: i32,
     character: &'a str,
-    password: &'a str
+    password: &'a str,
 }
 
 fn unpack_db_entry(db_entry: &str) -> DBEntry {
-    let entry_split = db_entry.split(":")
-        .collect::<Vec<&str>>();
+    let entry_split = db_entry.split(":").collect::<Vec<&str>>();
     let requirements = entry_split[0];
     let password = entry_split[1].trim();
-    let requirements = requirements.split(" ").map(|x| x.trim()).collect::<Vec<&str>>();
+    let requirements = requirements
+        .split(" ")
+        .map(|x| x.trim())
+        .collect::<Vec<&str>>();
     let character = requirements[1].trim();
-    let counts = requirements[0].split("-")
+    let counts = requirements[0]
+        .split("-")
         .map(|count| count.trim().parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
-    DBEntry{min:counts[0], max:counts[1], character:character, password:password}
+    DBEntry {
+        min: counts[0],
+        max: counts[1],
+        character: character,
+        password: password,
+    }
 }
 
 fn db_entries_from_input(input_data: &Vec<String>) -> Vec<DBEntry> {
-    input_data.into_iter()
-        .map(|db_entry|
-            unpack_db_entry(db_entry.as_str())
-        ).collect()
+    input_data
+        .into_iter()
+        .map(|db_entry| unpack_db_entry(db_entry.as_str()))
+        .collect()
 }
 
 fn is_good_entry_part1(entry: &DBEntry) -> bool {
     let occurrences = entry.password.matches(entry.character).count() as i32;
-    let valid_occurrences = (entry.min..entry.max+1).collect::<Vec<i32>>();
+    let valid_occurrences = (entry.min..entry.max + 1).collect::<Vec<i32>>();
     valid_occurrences.iter().any(|&i| i == occurrences)
 }
 
 fn n_good_part1(db_entries: &Vec<DBEntry>) -> u32 {
-    db_entries.into_iter()
+    db_entries
+        .into_iter()
         .filter(|entry| is_good_entry_part1(&entry))
         .collect::<Vec<&DBEntry>>()
         .len() as u32
 }
 
 fn is_good_entry_part2(entry: &DBEntry) -> bool {
-    (entry.password.chars().nth((entry.min-1) as usize).unwrap() == entry.character.chars().nth(0).unwrap())
-        ^ (entry.password.chars().nth((entry.max-1) as usize).unwrap() == entry.character.chars().nth(0).unwrap())
+    (entry
+        .password
+        .chars()
+        .nth((entry.min - 1) as usize)
+        .unwrap()
+        == entry.character.chars().nth(0).unwrap())
+        ^ (entry
+            .password
+            .chars()
+            .nth((entry.max - 1) as usize)
+            .unwrap()
+            == entry.character.chars().nth(0).unwrap())
 }
 
 fn n_good_part2(db_entries: &Vec<DBEntry>) -> u32 {
-    db_entries.into_iter()
+    db_entries
+        .into_iter()
         .filter(|entry| is_good_entry_part2(&entry))
         .collect::<Vec<&DBEntry>>()
         .len() as u32
