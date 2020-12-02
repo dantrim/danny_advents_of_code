22df5b808f0ebd671de3194b22577c441a14ef38
Daniel Antrim <dantrim@lbl.gov>
2020-12-02 08:55:35 -0800

    make file line parsing one liner

--- 2020/rust/day_01/src/main.rs
+++ 2020/rust/day_01/src/main.rs
@@ -8,25 +8,18 @@
 
 use clap::{App, Arg};
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 extern crate itertools;
 use itertools::Itertools;
 
-fn read_data_from_file(path: &str) -> Result<Vec<i32>, std::io::Error> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let mut v = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        let value: i32 = line
-            .trim()
-            .parse()
-            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
-        v.push(value);
-    }
-    Ok(v)
+fn read_data_from_file(path: &str) -> Vec<i32> {
+    let v = BufReader::new(File::open(path).unwrap())
+        .lines()
+        .map(|line| line.unwrap().trim().parse::<i32>().unwrap())
+        .collect();
+    v
 }
 
 fn day01(input_data: &Vec<i32>, n_for_combination: u32) -> i32 {
@@ -68,13 +61,7 @@ fn main() {
         std::process::exit(1);
     }
 
-    let input_data = match read_data_from_file(input) {
-        Err(e) => {
-            println!("Error reading data from input file: {}", e);
-            std::process::exit(1);
-        }
-        Ok(input_data) => input_data,
-    };
+    let input_data = read_data_from_file(input);
     let product_part1 = day01(&input_data, 2);
     let product_part2 = day01(&input_data, 3);
     println!("product_part1: {}", product_part1);
