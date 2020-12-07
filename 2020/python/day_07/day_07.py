a405e0d5a72ef3c282e893e7e9844835de74f4da
Daniel Antrim <dantrim@lbl.gov>
2020-12-07 07:38:10 -0800

    test

--- 2020/python/day_07/day_07.py
+++ 2020/python/day_07/day_07.py
@@ -8,11 +8,18 @@
 # e-mail: dantrim1023 AT gmail DOT com
 #
 
-# import sys
-# from argparse import ArgumentParser
-# from pathlib import Path
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
 
 # import pytest
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description="AoC day #7")
+    parser.add_argument("input", help="Day #7 input file")
+    args = parser.parse_args()
+    input_path = Path(args.input)
+    if not input_path.exists():
+        print(f"ERROR: bad input '{args.input}'")
+        sys.exit(1)
     pass
