9556cf1810ebff534f966663978523878027e6a3
Daniel Antrim <dantrim@lbl.gov>
2020-12-11 07:31:12 -0800

    start

---
+++ 2020/python/day_11/day_11.py
@@ -0,0 +1,117 @@
+#!/bin/env python
+
+#
+# Advent of Code 2020
+# Day 11
+#
+# author: Daniel Joseph Antrim
+# e-mail: dantrim1023 AT gmail DOT com
+#
+
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def example_data_part1():
+    test_data = [
+        """
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+    """,
+        """
+    #.##.##.##
+    #######.##
+    #.#.#..#..
+    ####.##.##
+    #.##.##.##
+    #.#####.##
+    ..#.#.....
+    ##########
+    #.######.#
+    #.#####.##
+    """,
+        """
+    #.LL.L#.##
+    #LLLLLL.L#
+    L.L.L..L..
+    #LLL.LL.L#
+    #.LL.LL.LL
+    #.LLLL#.##
+    ..L.L.....
+    #LLLLLLLL#
+    #.LLLLLL.L
+    #.#LLLL.##
+    """,
+        """
+    #.##.L#.##
+    #L###LL.L#
+    L.#.#..#..
+    #L##.##.L#
+    #.##.LL.LL
+    #.###L#.##
+    ..#.#.....
+    #L######L#
+    #.LL###L.L
+    #.#L###.##
+    """,
+        """
+    #.#L.L#.##
+    #LLL#LL.L#
+    L.L.L..#..
+    #LLL.##.L#
+    #.LL.LL.LL
+    #.LL#L#.##
+    ..L.L.....
+    #L#LLLL#L#
+    #.LLLLLL.L
+    #.#L#L#.##
+    """,
+        """
+    #.#L.L#.##
+    #LLL#LL.L#
+    L.#.L..#..
+    #L##.##.L#
+    #.#L.LL.LL
+    #.#L#L#.##
+    ..L.L.....
+    #L#L##L#L#
+    #.LLLLLL.L
+    #.#L#L#.##
+    """,
+    ]
+    return [[x.strip() for x in t.split("\n") if x] for t in test_data]
+
+
+def test_round_1(example_data_part1):
+    assert apply_rules(example_data_part1[0]) == example_data_part1[1]
+
+
+def apply_rules(seats):
+    return []
+
+
+def main(input_path):
+    pass
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="AoC day #11")
+    parser.add_argument("input", help="Day #11 input file")
+    args = parser.parse_args()
+    input_path = Path(args.input)
+    if not input_path.exists():
+        print(f"ERROR: bad input '{args.input}'")
+        sys.exit(1)
+    main(input_path)
