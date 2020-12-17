20c07f8bb78ee9bbc5a415ebe58fb8cfceba82a0
Daniel Antrim <dantrim@lbl.gov>
2020-12-16 23:04:03 -0800

    day 16 part 2 solution

--- 2020/python/day_16/day_16.py
+++ 2020/python/day_16/day_16.py
@@ -13,6 +13,7 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 
 import pytest
 
@@ -35,10 +36,35 @@ nearby tickets:
     return [x.strip() for x in test_data.split("\n") if x]
 
 
+@pytest.fixture
+def example_data_part2():
+    test_data = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+    return [x.strip() for x in test_data.split("\n") if x]
+
+
 def test_example_1(example_data):
 
     _, ranges, _, tickets = load_input_data(example_data)
-    assert part1(ranges, tickets) == 71
+    error_rate, _ = part1(ranges, tickets)
+    assert error_rate == 71
+
+
+def test_example_2(example_data_part2):
+    names, ranges, _, tickets = load_input_data(example_data_part2)
+    _, tickets = part1(ranges, tickets)
+    class_ordering = determine_class_assignment(names, ranges, tickets)
+    assert class_ordering == {0: "row", 1: "class", 2: "seat"}
 
 
 def load_input_data(input_data):
@@ -103,22 +129,99 @@ def part1(class_ranges, nearby_tickets):
     valid_values_any_field = set(valid_values_any_field)
 
     bad_values = []
-    for ticket in nearby_tickets:
-        ticket = set(ticket)
-        bad_values += list(ticket - valid_values_any_field)
-    return sum(bad_values)
+    ok_tickets = []
+    for iticket, ticket in enumerate(nearby_tickets):
+        ticket_set = set(ticket)
+        invalid_field_data = list(ticket_set - valid_values_any_field)
+        bad_values += invalid_field_data
+        if len(invalid_field_data) == 0:
+            ok_tickets.append(ticket)
+    return sum(bad_values), ok_tickets
+
+
+def determine_class_assignment(class_names, class_ranges, input_tickets):
+
+    max_len_class_range = -1
+    max_len_tickets = -1
+
+    for class_range in class_ranges:
+        max_len_class_range = max([max_len_class_range, len(class_range)])
+    for ticket in input_tickets:
+        max_len_tickets = max([max_len_tickets, len(ticket)])
+
+    ranges = []
+    for class_range in class_ranges:
+        while len(class_range) != max_len_class_range:
+            class_range.append(class_range[-1])
+        ranges.append(class_range)
+    tickets = []
+    for ticket in input_tickets:
+        while len(ticket) != max_len_tickets:
+            ticket.append(ticket[-1])
+        tickets.append(ticket)
+
+    ranges = np.array(
+        ranges
+    ).T  # take transpose to associate range rows to all tickets in a specific column
+    tickets = np.array(tickets)
+
+    # 2D array of class ranges
+    n_classes_from_tickets = tickets.shape[1]
+    n_classes_from_ranges = ranges.shape[1]
+    if n_classes_from_ranges != n_classes_from_tickets:
+        print(
+            f"ERROR different classes from ranges vs tickets (ranges: {n_classes_from_ranges}, tickets: {n_classes_from_tickets})"
+        )
+        sys.exit(1)
+
+    # use the hungarian algorithm (https://en.wikipedia.org/wiki/Hungarian_algorithm)
+    # to define a cost matrix for assigning ticket columns to class columns
+    n_classes = n_classes_from_ranges
+    cost_matrix = np.zeros(shape=(n_classes, n_classes))
+    for i in range(0, n_classes):
+        ticket_col = set(tickets[:, i])
+        for j in range(0, n_classes):
+            range_col = set(ranges[:, j])
+            diff = ticket_col - range_col
+
+            # for valid assignments, the cost should be minimal -- set it to zero
+            if not diff:
+                pass  # cost matrix is intially all zeros
+            else:
+                cost_matrix[i, j] = 1
+
+    row_assignments, col_assignments = linear_sum_assignment(cost_matrix)
+    assignments = list(zip(row_assignments, col_assignments))
+
+    col_to_class_map = {}
+    for assignment in assignments:
+        tickets_col, class_index = assignment
+        col_to_class_map[tickets_col] = class_names[class_index]
+    return col_to_class_map
 
 
 def main(input_path):
 
     with open(input_path, "r") as ifile:
         input_data = [x.strip() for x in ifile.readlines()]
+
     class_names, class_ranges, my_ticket, nearby_tickets = load_input_data(input_data)
     print(f"Loaded {len(class_names)} classes and {len(nearby_tickets)} nearby tickets")
 
     # part1
-    error_rate = part1(class_ranges, nearby_tickets)
+    error_rate, ok_nearby_tickets = part1(class_ranges, nearby_tickets)
     print(f"PART 1: ticket scanning error rate = {error_rate}")
+
+    # part 2
+    class_assignment = determine_class_assignment(
+        class_names, class_ranges, ok_nearby_tickets
+    )
+    departure_vals = []
+    for ticket_col, class_name in class_assignment.items():
+        if class_name.startswith("departure"):
+            departure_vals.append(my_ticket[ticket_col])
+    print(f"PART 2: departure vals = {departure_vals}")
+    print(f"PART 2: departure vals product = {np.prod(departure_vals)}")
 
 
 if __name__ == "__main__":
